128bc2967b7a1e0e77b5169b99140cec66842d0b
dongxiaohua <18600387814@163.com>
2019-01-15 10:40:30 +0800

    grpc

--- src/main/java/com/dxh/practice/grpc/GrpcService.java
+++ src/main/java/com/dxh/practice/grpc/GrpcService.java
@@ -16,6 +16,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * grpc服务端
  * @author dongxiaohua
  */
 @Component
--- src/main/java/com/dxh/practice/mapper/UserMapper.java
+++ src/main/java/com/dxh/practice/mapper/UserMapper.java
@@ -7,6 +7,9 @@ import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
+/**
+ * @author dongxiaohua
+ */
 public interface UserMapper {
 
   @Select("")
