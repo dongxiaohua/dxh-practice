2ffb91c1b08964d65bcfd02dbb3d3e4809d5f600
dongxiaohua <dong_healer@163.com>
2022-09-12 23:31:16 +0800

    本地提交

---
+++ src/main/java/com/dxh/practice/alibaba/base/BaseMapper.java
@@ -0,0 +1,8 @@
+package com.dxh.practice.alibaba.base;
+
+/**
+ * @author: dongxiaohua
+ * @date: 2022-09-12 23:30:46
+ */
+public interface BaseMapper {
+}
---
+++ src/main/java/com/dxh/practice/alibaba/domain/VoteDO.java
@@ -0,0 +1,8 @@
+package com.dxh.practice.alibaba.domain;
+
+/**
+ * @author: dongxiaohua
+ * @date: 2022-09-12 23:31:09
+ */
+public class VoteDO {
+}
